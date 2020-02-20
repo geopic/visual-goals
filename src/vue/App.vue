42fc9d55603071d6561300523070b079d56fb5e0
George Pickering <29524044+geopic@users.noreply.github.com>
2020-02-20 15:27:42 +0000

    Add date-fns, style header & footer, add printWidth to Prettier config

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -10,9 +10,11 @@
       <router-view />
     </main>
     <footer>
-      <div id="site-credits">{{ siteTitle }} &copy; 2020</div>
-      <div id="site-github-repo">
-        <a href="https://github.com/geopic/visual-goals">GitHub repository</a>
+      <div id="site-credits-and-repo">
+        <div id="site-credits">{{ siteTitle }} &copy; 2020</div>
+        <div id="site-github-repo">
+          <a href="https://github.com/geopic/visual-goals">GitHub repository</a>
+        </div>
       </div>
       <button type="button" @click="deleteSiteData">Delete site data</button>
     </footer>
@@ -35,4 +37,116 @@ export default class App extends Vue {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+#app {
+  align-items: start;
+  display: grid;
+  grid-template-rows: 50px auto 40px;
+  height: 100vh;
+
+  & > * {
+    height: 100%;
+  }
+
+  header {
+    align-items: center;
+    background-color: yellow;
+    display: flex;
+    font-size: 22px;
+    justify-content: space-around;
+
+    #site-title {
+      flex: 0 1 60%;
+      text-align: center;
+    }
+
+    nav {
+      display: none; /** Remove if more pages are needed */
+      flex: 0 1 40%;
+      height: 100%;
+
+      a {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+        height: 100%;
+        padding: 0px 10px;
+
+        &:active {
+          background-color: rgba(0, 0, 0, 0.2);
+        }
+      }
+    }
+
+    @media all and (min-width: $medquery-min-width-01) {
+      font-size: 24px;
+    }
+
+    @media all and (min-width: $medquery-min-width-02) {
+      font-size: 26px;
+    }
+
+    @media all and (min-width: $medquery-min-width-03) {
+      font-size: 28px;
+    }
+  }
+
+  main {
+    background-color: lightyellow;
+    border: 1px solid black;
+    border-left: 0px;
+    border-right: 0px;
+  }
+
+  footer {
+    align-items: stretch;
+    background-color: yellow;
+    display: flex;
+    font-size: 14px;
+    justify-content: space-between;
+
+    #site-credits-and-repo {
+      align-items: center;
+      display: flex;
+      flex: 0 1 70%;
+      flex-direction: column;
+      justify-content: center;
+    }
+
+    button {
+      background-color: firebrick;
+      border-color: transparent;
+      color: white;
+      cursor: pointer;
+      flex: 0 1 30%;
+      height: 100%;
+
+      &:active {
+        background-color: #851a1a;
+        color: #f0f0f0;
+      }
+    }
+
+    @media all and (min-width: $medquery-min-width-01) {
+      font-size: 16px;
+
+      #site-credits-and-repo {
+        flex-direction: row;
+        justify-content: space-evenly;
+      }
+    }
+
+    @media all and (min-width: $medquery-min-width-02) {
+      font-size: 18px;
+    }
+  }
+
+  @media all and (min-width: $medquery-min-width-04) {
+    border: 1px solid black;
+    border-bottom: 0px;
+    border-top: 0px;
+    margin: 0px auto;
+    width: $medquery-min-width-04;
+  }
+}
+</style>
--- src/vue/views/Home.vue
+++ src/vue/views/Home.vue
@@ -4,16 +4,27 @@
     <template v-else>
       <form @submit.prevent="initData" method="post">
         <div class="form-section">
-          <h1>1. Define a goal</h1>
-          <input type="text" name="goal-name" />
+          <label for="goal-name">
+            <h1>1. Define a goal</h1>
+          </label>
+          <input type="text" id="goal-name" />
         </div>
         <div class="form-section">
-          <h1>2. Set the starting date</h1>
-          <input type="date" name="goal-start-date" />
+          <label for="goal-start-date">
+            <h1>2. Set the starting date</h1>
+          </label>
+          <input type="date" id="goal-start-date" :value="today" />
         </div>
         <div class="form-section">
-          <h1>3. Set the ending date</h1>
-          <input type="date" name="goal-end-date" />
+          <label for="goal-end-date">
+            <h1>3. Set the ending date</h1>
+          </label>
+          <input
+            type="date"
+            id="goal-end-date"
+            :value="defaultEndDate"
+            :min="today"
+          />
         </div>
         <div class="form-section">
           <button type="submit">Get started!</button>
@@ -26,9 +37,13 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { SiteData } from '@/common/types';
+import { addDays, format } from 'date-fns';
 
 @Component
 export default class Home extends Vue {
+  today = format(new Date(), 'yyyy-MM-dd');
+  defaultEndDate = format(addDays(new Date(this.today), 14), 'yyyy-MM-dd');
+
   get dataExists() {
     return this.$store.state.dataExists;
   }
@@ -47,3 +62,16 @@ export default class Home extends Vue {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+#home {
+  form {
+    .form-section {
+      align-items: center;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+  }
+}
+</style>
