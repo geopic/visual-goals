459312b400d235aaa5c19650c85b82acb676fb84
George Pickering <29524044+geopic@users.noreply.github.com>
2020-02-23 17:52:15 +0000

    Rename GoalDisplay to DayDisplay, set up basic component

---
+++ src/vue/components/DayDisplay.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="day-display">
+    <h1>{{ formattedDate }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { format } from 'date-fns';
+
+@Component
+export default class DayDisplay extends Vue {
+  @Prop() date!: string;
+
+  formattedDate = format(new Date(this.date), 'iii. do LLL yyyy');
+}
+</script>
+
+<style lang="scss" scoped></style>
